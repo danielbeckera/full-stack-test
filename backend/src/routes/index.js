c1a56fad5ee3b8829eb6f19842dfdc2eb3106d21
Daniel Becker <danielbeckeralves@gmail.com>
2023-07-18 16:14:16 -0300

    added login route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
+const sessionRouter = require("./session");
 const userRouter = require("./user");
 
 const routes = Router();
 
+routes.use("/login", sessionRouter);
 routes.use("/user", userRouter);
 
 module.exports = routes;
