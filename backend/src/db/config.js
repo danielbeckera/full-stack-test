482b19235824101543e1c2a961c65052e5f64e04
Daniel Becker <danielbeckeralves@gmail.com>
2023-07-18 14:16:22 -0300

    changed config from js to json to fix reading models

--- backend/src/db/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  development: {
-    username: "postgres",
-    password: "mytapp_password",
-    database: "postgres",
-    host: "localhost",
-    dialect: "postgres",
-  },
-};
