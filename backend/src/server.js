ae14952a4dc3f73c9856901d346bdaf0af32bdc5
Daniel Becker <danielbeckeralves@gmail.com>
2023-07-18 11:45:41 -0300

    sync db on server start

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const initializeServer = async () => {
+  const db = require("./db");
+
   const app = express();
 
   app.use(express.json());
+
+  await db.sequelize.sync();
 
   app.listen(3001, () => {
     console.log("server is running on port 3001");
