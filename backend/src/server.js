a7a8d086afb9d034520b580adbfada4e769cbf2e
Daniel Becker <danielbeckeralves@gmail.com>
2023-07-18 12:03:03 -0300

    added cors and routes to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
+const routes = require("./routes");
 
 const initializeServer = async () => {
   const db = require("./db");
 
   const app = express();
 
+  app.use(cors());
   app.use(express.json());
+  app.use(routes);
 
   await db.sequelize.sync();
 
