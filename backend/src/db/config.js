96e4bb5ea5d90a6836262364f5646850262c3129
Daniel Becker <danielbeckeralves@gmail.com>
2023-07-18 11:47:02 -0300

    added pg settings and sequelize

---
+++ backend/src/db/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "mytapp_password",
+    database: "postgres",
+    host: "localhost",
+    dialect: "postgres",
+  },
+};
