c55f2275366b06a2367b13577cc70eac41f77024
Daniel Becker <danielbeckeralves@gmail.com>
2023-07-19 00:11:51 -0300

    added domains for images allowed

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["images.punkapi.com", "gothops.blog"],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
